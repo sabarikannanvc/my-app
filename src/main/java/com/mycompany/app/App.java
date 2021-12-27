4755a2728a0246cd48713a869ae8882f018d5986
sabarikannanvc <96328476+sabarikannanvc@users.noreply.github.com>
2021-12-26 22:04:17 -0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World All!" );
-	System.out.println( "Hi") ;
+	System.out.println( "Hi friends") ;
 	System.out.println(" Testing freestyle configure and  build");
     }
 }
