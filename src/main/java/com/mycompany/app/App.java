16562be53b9257d5810d7d04a44a2a90c9b19a3e
sabari kannan <5085sabarikannanvc@gmail.com>
2021-12-31 08:48:14 -0800

    changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,14 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        int x = 15;
-=======
+
+        int x = 20;
+
         System.out.println( "Hello World All!" );
 	System.out.println( "Hi friends") ;
 	System.out.println(" Testing freestyle configure and  build");
 	    System.out.println("Hei");
 	    
->>>>>>> 35820f27da73e0b126cc625d809a61039f8dbdb8
     }
 }
